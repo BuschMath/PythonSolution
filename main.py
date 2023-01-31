28accf86425c94c40092d11c9e6e5f069b9edd3e
BuschMath <busch@iowacentral.edu>
2023-01-31 15:20:32 -0600

    Tables inputed

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ with open('customer.txt') as inFile:
     linesOfInputFile = inFile.readlines()
     inFile.close()
 
-print(linesOfInputFile[0])
-print(linesOfInputFile[1])
-
 dashes = linesOfInputFile[1].split()
 lenDashes = [len(dashes[0])]
 
 for i in range (1, len(dashes),):
     lenDashes.append(lenDashes[i-1] + len(dashes[i]) + 1)
-
-print(lenDashes)
 
 lCustomers = []
 line = []
@@ -22,10 +17,32 @@ for i in range (2, len(linesOfInputFile), ):
     for j in range(1, len(lenDashes), ):
         line.append(linesOfInputFile[i][lenDashes[j-1]:lenDashes[j]])
 
-    print(line)
-    lCustomers.append(dict(uname=line[0], passwd=line[1], lname=line[2], 
-        fname=line[3], addr=line[4], zone=line[5], sex=line[6], age=line[7], 
-        limit=line[8], balance=line[9], creditcard=line[10], email=line[11], 
-        active=line[12]))
+    lCustomers.append(dict(uname=line[0].strip(), passwd=line[1].strip(), 
+        lname=line[2].strip(), fname=line[3].strip(), addr=line[4].split(), 
+        zone=line[5].strip(), sex=line[6].strip(), age=line[7].strip(), 
+        limit=line[8].strip(), balance=line[9].strip(), creditcard=line[10].strip(), 
+        email=line[11].strip(), active=line[12].strip()))
 
-print(lCustomers)
+with open('zonecost.txt') as inFile:
+    linesOfInputFile = inFile.readlines()
+    inFile.close()
+
+dashes = linesOfInputFile[1].split()
+lenDashes = [len(dashes[0])]
+
+for i in range (1, len(dashes),):
+    lenDashes.append(lenDashes[i-1] + len(dashes[i]) + 1)
+
+lZone = []
+line = []
+
+for i in range (2, len(linesOfInputFile), ):
+    line.clear()
+    line.append(linesOfInputFile[i][0:lenDashes[0]])
+    for j in range(1, len(lenDashes), ):
+        line.append(linesOfInputFile[i][lenDashes[j-1]:lenDashes[j]])
+
+    lZone.append(dict(zoneid=line[0].strip(), zonedesc=line[1].strip(), 
+        price=line[2].strip()))
+
+print(lZone)
